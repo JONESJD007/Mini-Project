5e1da30f465836ee25bd6db69505c39d7f733d56
Jones Jeffry D <137525887+JONESJD007@users.noreply.github.com>
2023-10-13 23:17:59 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+// Define the two numbers to be summed
+const num1 = 5;
+const num2 = 7;
+
+// Calculate the sum
+const sum = num1 + num2;
+
+// Display the result
+console.log("The sum is: " + sum);
